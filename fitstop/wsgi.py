c5cf05ea864c6610a0f42d4baf9292035b3ff8e3
Hephzi <52066666+hephzibahponcellat@users.noreply.github.com>
2021-03-15 00:53:10 +0530

    Add files via upload

--- fitstop/wsgi.py
+++ fitstop/wsgi.py
@@ -1,1 +1,16 @@
+"""
+WSGI config for fitstop project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitstop.settings')
+
+application = get_wsgi_application()
